52dac1710d1f218e82d31e63fc886e9640ed2f69
TheoCollignon <43596225+TheoCollignon@users.noreply.github.com>
2020-05-20 16:37:57 +0200

    fichier de Configuration

---
+++ src/Configuration.java
@@ -0,0 +1,7 @@
+public class Configuration {
+    public static int NBR_INTERFACES = 10;
+    public static int NBR_APPRENANTS = 8;
+    public static int NBR_CENTRES_FORMATION = 3;
+    public static int NBR_SPECIALITES = 3;
+    public static int NBR_NODES = NBR_CENTRES_FORMATION+NBR_INTERFACES+NBR_APPRENANTS;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+
     public static void main(String[] args) {
+        Configuration config = new Configuration();
         System.out.println("Problème d'optimisation");
     }
 }
