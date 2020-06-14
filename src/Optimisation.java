9d94cae7eeb1abf710e692efbc7d862198bdecdc
LucasPARISOT <lucas.parisot@utbm.fr>
2020-06-14 15:59:43 +0200

    Doc Optimisation

--- src/Optimisation.java
+++ src/Optimisation.java
@@ -30,17 +30,21 @@ public class Optimisation {
     }
 
     public void begin() {
-
         System.out.println("        Date        |     Spécialité     |     Compétence     |      Interface     |      Apprenant     ");
 
-        int id = 0;
+        // Itere les jours de la semaine
         for (int day = 1; day < 7; day++) {
 
+            // Lie les formations a la journee
             ArrayList<Formation> formationsDuJour = new ArrayList<>();
             for (Formation f : formations) {
                 if (f.getJour() == day) formationsDuJour.add(f);
             }
+
+            // Trie les formations de la journee par heure de debut
             formationsDuJour.sort(Comparator.comparingInt(Formation::getHeureDebut));
+
+            // Init les interfaces
             for (Interface i : interfaces) {
                 i.setValeurJour(0);
                 i.setTempsTravailJour(0);
@@ -48,10 +52,9 @@ public class Optimisation {
 
             for (Formation f : formationsDuJour) {
 
-                //printer.printFormation(f, id);
-                //System.out.println("Interfaces compatibles : ");
                 ArrayList<Interface> interfacesCompatibles = new ArrayList<>();
 
+                // Verification de la competence et de l'heure
                 for (Interface i : interfaces) {
 
                     if ((i.getIdCompetence() == f.getIdCompetence())
@@ -61,10 +64,10 @@ public class Optimisation {
                     }
                 }
 
+                // Calcul de la valeur du jour pour chaque interface compatible et multiplicateurs
                 for (Interface i : interfacesCompatibles) {
 
                     i.setDistanceTemporaire(0);
-                    //printer.printInterface(i);
 
                     // Domicile interface -> SESSAD
                     int idInterface = i.getId();
@@ -72,10 +75,6 @@ public class Optimisation {
                     double distanceInterfaceSESSAD = calcDistance(coordInterface, coordSESSAD);
                     i.incrDistanceTemporaire(distanceInterfaceSESSAD);
                     i.incrValeurJour(distanceInterfaceSESSAD);
-                    /*
-                    System.out.println(idInterface);
-                    System.out.println(coordInterface.getX());
-                    */
 
                     // SESSAD -> Domicile apprenant
                     int idApprenant = f.getIdAprennant();
@@ -102,39 +101,19 @@ public class Optimisation {
                     i.incrDistanceTemporaire(distanceInterfaceSESSAD);
                     i.incrValeurJour(distanceInterfaceSESSAD);
 
-                    //TODO: Multiplier la valeur du jour actuelle (que distance) par le multiplicateur de distance
-                    //TODO: Multiplicateur heure de travail (MALUS plus le nombre d'heure est important)
-
-                    //System.out.println("avant distance "+i.getValeurJour());
-                    //System.out.println("avant distance "+i.getValeurJour() +" * "+calcMultiplicateurDistance(i) + " = " + i.getValeurJour()*calcMultiplicateurDistance(i));
                     i.setValeurJour(calcMultiplicateurDistance(i));
-                    //System.out.println("avant horaire "+i.getValeurJour());
                     i.setValeurJour(i.getValeurJour()*calcMultiplicateurHoraire(i));
                     //Multiplicateur de spécialité
-                    //System.out.println("avant formation "+i.getValeurJour());
                     if (i.getIdSpecialite() == f.getIdSpecialite()) {
-                        //System.out.println("Même spécialité ! / ");
                         i.setValeurJour(i.getValeurJour()*multiplicateurFormation);
                     }
-                    //System.out.println("final "+i.getValeurJour());
-                    /*else {
-                        System.out.println("Autre spécialité ! / ");
-                    }*/
-
-                    //System.out.println();
                 }
+
                 // On trie la liste d'interface par sa valeur
                 interfacesCompatibles.sort(Comparator.comparingDouble(Interface::getValeurJour).reversed());
 
-
-                //Verification sucess: Max to Min
-                /*System.out.println("0:" + interfacesCompatibles.get(0).getValeurJour());
-                System.out.println("1:" + interfacesCompatibles.get(1).getValeurJour());
-                System.out.println("2:" + interfacesCompatibles.get(2).getValeurJour());*/
-
-
-                for(int i = 1; i < interfacesCompatibles.size(); i++)
-                {
+                // Reinitialisation des interface, sauf celle choisie
+                for(int i = 1; i < interfacesCompatibles.size(); i++) {
                     // On reinitialise la variable pour les interfaces non choisis
                     interfacesCompatibles.get(i).setValeurJour(0);
                 }
@@ -144,19 +123,12 @@ public class Optimisation {
                     interfacesCompatibles.get(0).incrTempsTravail(f.getDuree());
                     interfacesCompatibles.get(0).incrDistanceTotale(interfacesCompatibles.get(0).getDistanceTemporaire());
                     printer.selection(f, interfacesCompatibles.get(0));
-                } else {
-                    System.out.println("eh y'a rien");
                 }
-
-                //printer.printInterface(interfacesCompatibles.get(0));
-
-
             }
         }
     }
 
     private double calcMultiplicateurDistance(Interface i) {
-        //System.out.println(i.getValeurJour()/ (dimension*5));
         return (dimension*5) / i.getValeurJour();
     }
 
@@ -164,7 +136,6 @@ public class Optimisation {
         if ((double)i.getTempsTravail() / 35 == 0) {
             return 1;
         } else {
-            //System.out.println((double)i.getTempsTravail()/35);
             return (double)i.getTempsTravail()/35;
         }
     }
